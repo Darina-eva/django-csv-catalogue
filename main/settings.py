745d16a67d62ea5628d58b29ef7df407722bc553
Darina <darina.eva00.00@mail.ru>
2026-01-21 20:08:03 +0300

    chore: remove comment

--- main/settings.py
+++ main/settings.py
@@ -52,16 +52,6 @@ MIDDLEWARE = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'netology_import_phones',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
